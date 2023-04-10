d66cd4ca88768d8fe6a641ffd6a21bf95ae34895
Jõao Eduardo <joaoeduardo.lsilva@gmail.com>
2023-04-10 01:35:29 -0300

    Atribuindo valores

--- src/PriProjeto.java
+++ src/PriProjeto.java
@@ -1,11 +1,16 @@
-
+//ATRIBUINDO VALORES
 public class PriProjeto {
  public static void main (String[] args){
     System.out.println("Esse e o screen match");
     System.out.println("Filme: Homen Aranha: Marvel Studio");
-
+    
+    //CHAMANDO A VARIAVEL
     int anoDeLancamento = 2022;
     System.out.println("Ano de Lancamento:" + anoDeLancamento);
+    
+    //variavel que assume os valores true ou false
+    boolean IncluidoNoPlano = true;
+
 }
 }
  
