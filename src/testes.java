5776b6f02fdf8541204f114c8707d95d27fda75e
Jõao Eduardo <joaoeduardo.lsilva@gmail.com>
2023-04-22 03:38:25 -0300

    treinamento de true false

--- src/testes.java
+++ src/testes.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class testes {
+//Para poder fazer digitacao nao terminal esse e o format cod.
     public static void main(String[] args) {
         try (Scanner testes = new Scanner(System.in)) {
             System.out.print("Digite seu nome: ");
@@ -8,7 +9,7 @@ public class testes {
             System.out.println();
 
             
-        
+//Para poder usar condicoes em strings e essa a forma
     if(nome.equals("joao") )
     {
            System.out.println("yes ok ok !!!");
