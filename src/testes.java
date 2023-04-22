12f68968b162daa9f3f1af87277544a020831aca
Jõao Eduardo <joaoeduardo.lsilva@gmail.com>
2023-04-22 03:35:12 -0300

    treinamento de true false

---
+++ src/testes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class testes {
+    public static void main(String[] args) {
+        try (Scanner testes = new Scanner(System.in)) {
+            System.out.print("Digite seu nome: ");
+            String nome = testes.nextLine();
+            System.out.println();
+
+            
+        
+    if(nome.equals("joao") )
+    {
+           System.out.println("yes ok ok !!!");
+    }
+    else {
+       System.out.println("what is yor name?"); 
+         }   
+       }
+    }
+}
