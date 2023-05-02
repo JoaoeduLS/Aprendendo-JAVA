f5b96be918980fd32c1364546d1a35e026008321
JoaoeduLS <joaoeduardo.lsilva@gmail.com>
2023-05-02 11:52:09 -0300

    desafio jogo de sorteio testetwo

---
+++ src/TesteTwo.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TesteTwo {
+    public static void main(String[] args) {
+        Scanner leitura=new Scanner(System.in);
+        int aleatoria = new Random().nextInt(100);
+        System.out.println("qual avaliacao voce da a ao filme:");
+        
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Adivinhe o número tentativa " + i);
+            int numero = leitura.nextInt();
+
+            if (numero == aleatoria) {
+                System.out.println("voce ganho");
+            }
+            else if(numero < aleatoria){
+                System.out.println("tente um numero maior");
+            }
+            else if(numero > aleatoria){
+                System.out.println("tente um numero menor");
+            }
+        }
+        System.out.println("Número sorteado "+ aleatoria);
+    }
+
+    
+    
+    }
+    
+
